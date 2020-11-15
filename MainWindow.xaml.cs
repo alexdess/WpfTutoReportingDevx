a885bebd9f5e13da27b5f48444c201a52338fb14
Alexandre Dessonnaz <alexandre_dess@hotmail.com>
2020-11-15 21:14:14 +0100

    connection string

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,24 @@ namespace WpfTutoReportingDevx
         public MainWindow()
         {
             InitializeComponent();
+            var mysqlConnection = DatabaseConnection.getDBConnection();
+            mysqlConnection.Open();
+            var version = mysqlConnection.ServerVersion;
+            Console.WriteLine("MySQL version: " + version);
+        }
+
+        public static class DatabaseConnection
+        {
+            static MySqlConnection databaseConnection = null;
+            public static MySqlConnection getDBConnection()
+            {
+                if (databaseConnection == null)
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["myDatabaseConnection"].ConnectionString;
+                    databaseConnection = new MySqlConnection(connectionString);
+                }
+                return databaseConnection;
+            }
         }
     }
 }
