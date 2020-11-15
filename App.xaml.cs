a48f2c598613c4bd0ec1c912a8f98223cea22b4e
Alexandre Dessonnaz <alexandre_dess@hotmail.com>
2020-11-15 23:19:12 +0100

    skeleton updated

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,6 @@ namespace WpfTutoReportingDevx
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,7 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DevExpress.XtraReports.UI;
+using DevExpress.Xpf.Printing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System;
 
 namespace WpfTutoReportingDevx
 {
@@ -25,24 +13,29 @@ namespace WpfTutoReportingDevx
         public MainWindow()
         {
             InitializeComponent();
-            var mysqlConnection = DatabaseConnection.getDBConnection();
-            mysqlConnection.Open();
-            var version = mysqlConnection.ServerVersion;
-            Console.WriteLine("MySQL version: " + version);
+            //var mysqlConnection = DatabaseConnection.getDBConnection();
+            //mysqlConnection.Open();
+            //var version = mysqlConnection.ServerVersion;
+            //Console.WriteLine("MySQL version: " + version);
         }
 
-        public static class DatabaseConnection
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            static MySqlConnection databaseConnection = null;
-            public static MySqlConnection getDBConnection()
-            {
-                if (databaseConnection == null)
-                {
-                    string connectionString = ConfigurationManager.ConnectionStrings["myDatabaseConnection"].ConnectionString;
-                    databaseConnection = new MySqlConnection(connectionString);
-                }
-                return databaseConnection;
-            }
+            PrintHelper.ShowPrintPreview(null, new XtraReport3());
         }
+
+        //public static class DatabaseConnection
+        //{
+        //    static MySqlConnection databaseConnection = null;
+        //    public static MySqlConnection getDBConnection()
+        //    {
+        //        if (databaseConnection == null)
+        //        {
+        //            string connectionString = ConfigurationManager.ConnectionStrings["myDatabaseConnection"].ConnectionString;
+        //            databaseConnection = new MySqlConnection(connectionString);
+        //        }
+        //        return databaseConnection;
+        //    }
+
     }
 }
